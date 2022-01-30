c02558e72064129e61f195f6515164e2bcb12cdc
novarhar <94405646+novarhar@users.noreply.github.com>
2022-01-30 16:25:15 -0500

    Update Camera Measurement Program V1.java

--- Camera Measurement Program V1.java
+++ Camera Measurement Program V1.java
@@ -3,17 +3,17 @@ public class Distancefinderfinalteam467 {
     
     public static void main(String args[]){
         
-       double horizontalMiddleOfBoundingBox = //Middle point of the bounding box in terms of x (horizontal)
+       double horizontalMiddleOfBoundingBox = //Middle point of the bounding box in terms of x (horizontal) //(INTEGREATION)
 		   
-       double verticalMiddleOfBoundingBox = //Middle point of the bounding box in terms of y (vertical)
+       double verticalMiddleOfBoundingBox = //Middle point of the bounding box in terms of y (vertical) //(INTEGREATION)
        
        double horizontalMiddleOfCamera = 640 //Middle point of the Camera frame in terms of horizontal
 		   
        double verticalMiddleOfCamera = 360 //Middle point of the Camera frame in terms of vertical
 		   
-       double heightOfCameraOffTheGround = //height of camera off the ground
+       double heightOfCameraOffTheGround = //height of camera off the ground //(MEASUREMENT)
 		   
-       double heightOfTowerTarget = //constant of height of tower
+       double heightOfTowerTarget = //constant of height of tower 
 		   
        //double differenceOftowerHeightandCameraHeight = //towerHeight-camHeight (opposite side of camera angle)
 		   
