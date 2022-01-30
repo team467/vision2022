bd08c97dfd14ec883c82b410b8c482d18382d801
novarhar <94405646+novarhar@users.noreply.github.com>
2022-01-30 14:02:06 -0500

    Create Camera Measurement Program V1.java

---
+++ Camera Measurement Program V1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Distancefinderfinalteam467 {
+    
+    public static void main(String args[]){
+        
+       double horizontalMiddleOfBoundingBox = //Middle point of the bounding box in terms of x (horizontal)
+		   
+       double verticalMiddleOfBoundingBox = //Middle point of the bounding box in terms of y (vertical)
+       
+       double horizontalMiddleOfCamera = 640 //Middle point of the Camera frame in terms of horizontal
+		   
+       double verticalMiddleOfCamera = 360 //Middle point of the Camera frame in terms of vertical
+		   
+       double heightOfCameraOffTheGround = //height of camera off the ground
+		   
+       double heightOfTowerTarget = //constant of height of tower
+		   
+       //double differenceOftowerHeightandCameraHeight = //towerHeight-camHeight (opposite side of camera angle)
+		   
+       double verticalAngleOfCameraFov = //*will determine ourselves* *whiteboard method*
+		   
+       double angleofTargetOffHorizontalMid = //Pixel-Angle ratio that determines the angle of bbMidy from camMidy
+       //VertVarAngle = angleofTargetOffHorizontalMid
+		   
+       double tanValue = Math.tan(angleofTargetOffHorizontalMid); // 
+		
+       double distanceOfRobotFromTarget = oppCamAng/tanValue; // oppCamAng/tan(VertVarAngle)
+ 
+       double horizontalAngleOfCameraFov = //*will determine ourselves* *white board method*
+		   
+       double angleofTargetOffVerticalMid = //using pixel-angle ratio that determines the angle of bbMidx from camMidx
+    }
+}
