42d9395ec9c47f9c38d00604c8ff9bf87a00868b
JashKotadiya <jash.kotadiya375@gmail.com>
2022-02-05 15:06:24 -0500

    Cleaned up with Final Contours and Distance

--- src/main/java/processing/HubTargetProcessor.java
+++ src/main/java/processing/HubTargetProcessor.java
@@ -11,8 +11,8 @@ import pipelines.HubTargetPipeline;
 public class HubTargetProcessor extends Processor {
 
     public static final String NETWORK_TABLE_NAME = "hub_target";
-    public static final double TARGET_HEIGHT_FT = 1.4583; //8.700;
-    public static final double CAMERA_HEIGHT_FT = 0.83; //2.875;
+    public static final double TARGET_HEIGHT_FT = 1.4583; // 8.700;
+    public static final double CAMERA_HEIGHT_FT = 0.83; // 2.875;
     public static final double UP_ANGLE_DEG = 0.0;// 45.0;
     public static final double TURN_ANGLE_OFFSET_DEG = 0.0;
 
@@ -42,11 +42,9 @@ public class HubTargetProcessor extends Processor {
                 beforeMean += box.y;
                 i++;
             }
-    
+
             beforeMean /= i;
-    
-            
-    
+
             i = 0;
             for (MatOfPoint contour : hubTargetPipeline.filterContoursOutput()) {
                 Rect box = Imgproc.boundingRect(contour);
@@ -56,35 +54,27 @@ public class HubTargetProcessor extends Processor {
                     i++;
                 }
             }
-    
+
             x /= i;
             y /= i;
-    
-        
+
             calcDistance(y);
             calcAngle(x);
             table.getEntry("isValid").setBoolean(true);
 
-        } else{
-            
+        } else {
+
             table.getEntry("angle").setDouble(0.0);
             table.getEntry("distance").setDouble(0.0);
             table.getEntry("isValid").setBoolean(false);
 
-
-
-
         }
-       
-        
-    }
 
+    }
 
     public void calcAngle(int x) {
 
-        double angle = (x - CAMERA_X_RESOLUTION / 2.0)
-                / PIXELS_PER_DEGREE
-                + TURN_ANGLE_OFFSET_DEG;
+        double angle = (x - CAMERA_X_RESOLUTION / 2.0) / PIXELS_PER_DEGREE + TURN_ANGLE_OFFSET_DEG;
         table.getEntry("angle").setDouble(angle);
         System.out.println(" Angle " + angle);
 
@@ -92,10 +82,8 @@ public class HubTargetProcessor extends Processor {
 
     public void calcDistance(int y) {
 
-        double distance = (TARGET_HEIGHT_FT - CAMERA_HEIGHT_FT) /
-        Math.tan((((CAMERA_Y_RESOLUTION / 2.0) - y)
-                / PIXELS_PER_DEGREE + UP_ANGLE_DEG)
-                * DEG_TO_RADIANS);
+        double distance = (TARGET_HEIGHT_FT - CAMERA_HEIGHT_FT)
+                / Math.tan((((CAMERA_Y_RESOLUTION / 2.0) - y) / PIXELS_PER_DEGREE + UP_ANGLE_DEG) * DEG_TO_RADIANS);
         table.getEntry("distance").setDouble(distance);
         System.out.println(" Distance " + distance);
 
