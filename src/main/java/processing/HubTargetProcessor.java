14f03717f203c66b7bc393c52b1d3a79455253de
JashKotadiya <jash.kotadiya375@gmail.com>
2022-02-05 15:02:05 -0500

    FinalContours and DistanceCalc

--- src/main/java/processing/HubTargetProcessor.java
+++ src/main/java/processing/HubTargetProcessor.java
@@ -8,17 +8,20 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.vision.VisionPipeline;
 import pipelines.HubTargetPipeline;
-
-public class HubTargetProcessor {
+public class HubTargetProcessor extends Processor {
 
     public static final String NETWORK_TABLE_NAME = "hub_target";
+    public static final double TARGET_HEIGHT_FT = 1.4583; //8.700;
+    public static final double CAMERA_HEIGHT_FT = 0.83; //2.875;
+    public static final double UP_ANGLE_DEG = 0.0;// 45.0;
+    public static final double TURN_ANGLE_OFFSET_DEG = 0.0;
 
     public static final int RANGE = 50;
 
     private NetworkTable table;
 
     public HubTargetProcessor(NetworkTableInstance networkTableInstance) {
-        NetworkTable smartDashboard = networkTableInstance.getTable("SmartDashboard");
+        super(networkTableInstance);
         table = smartDashboard.getSubTable(NETWORK_TABLE_NAME);
     }
 
@@ -30,55 +33,71 @@ public class HubTargetProcessor {
 
         System.out.println("Filter Counters - IN:" + hubTargetPipeline.findContoursOutput().size() + " OUT:"
                 + hubTargetPipeline.filterContoursOutput().size());
-        int i = 0;
-        int beforeMean = 0;
-        for (MatOfPoint contour : hubTargetPipeline.filterContoursOutput()) {
-            Rect box = Imgproc.boundingRect(contour);
-            beforeMean += box.y;
-            i++;
-        }
 
-        beforeMean /= i;
-
-        System.out.println(" beforeMain " + beforeMean);
-
-        i = 0;
-        for (MatOfPoint contour : hubTargetPipeline.filterContoursOutput()) {
-            Rect box = Imgproc.boundingRect(contour);
-            if (Math.abs(beforeMean - box.y) < RANGE) {
-                x += box.x;
-                y += box.y;
+        if (hubTargetPipeline.filterContoursOutput().size() != 0) {
+            int i = 0;
+            int beforeMean = 0;
+            for (MatOfPoint contour : hubTargetPipeline.filterContoursOutput()) {
+                Rect box = Imgproc.boundingRect(contour);
+                beforeMean += box.y;
                 i++;
             }
+    
+            beforeMean /= i;
+    
+            
+    
+            i = 0;
+            for (MatOfPoint contour : hubTargetPipeline.filterContoursOutput()) {
+                Rect box = Imgproc.boundingRect(contour);
+                if (Math.abs(beforeMean - box.y) < RANGE) {
+                    x += box.x;
+                    y += box.y;
+                    i++;
+                }
+            }
+    
+            x /= i;
+            y /= i;
+    
+        
+            calcDistance(y);
+            calcAngle(x);
+            table.getEntry("isValid").setBoolean(true);
+
+        } else{
+            
+            table.getEntry("angle").setDouble(0.0);
+            table.getEntry("distance").setDouble(0.0);
+            table.getEntry("isValid").setBoolean(false);
+
+
+
+
         }
-
-        x /= i;
-        y /= i;
-
-        System.out.println(" X " + x + "; Y " + y );
-
-        calcDistance(y);
-        calcAngle(x);
-
+       
+        
     }
+
 
     public void calcAngle(int x) {
 
-        double angle = 0.0;
-
-        // TODO: Calculate Distance and Angle
-
+        double angle = (x - CAMERA_X_RESOLUTION / 2.0)
+                / PIXELS_PER_DEGREE
+                + TURN_ANGLE_OFFSET_DEG;
         table.getEntry("angle").setDouble(angle);
+        System.out.println(" Angle " + angle);
 
     }
 
     public void calcDistance(int y) {
 
-        double distance = 0.0;
-
-        // TODO: Calculate Distance and Angle
-
+        double distance = (TARGET_HEIGHT_FT - CAMERA_HEIGHT_FT) /
+        Math.tan((((CAMERA_Y_RESOLUTION / 2.0) - y)
+                / PIXELS_PER_DEGREE + UP_ANGLE_DEG)
+                * DEG_TO_RADIANS);
         table.getEntry("distance").setDouble(distance);
+        System.out.println(" Distance " + distance);
 
     }
 }
